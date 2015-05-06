d4d542235de3c1133616c901ea5d9e79212a7b08
MathieuGi <math.girardon@hotmail.fr>
2015-05-06 14:59:30 +0200

    header
    onglet connection

---
+++ Header/connection_non_connecte.php
@@ -0,0 +1,14 @@
+<article>
+	<p>
+		Bienvenu <?php echo '$pseudo' ?>
+	</p>
+	<a href="#"> 
+		<p> Mon compte </p> 
+	</a>
+	<p>
+		<a href="##"> 
+			<img src="../images/panier.png" alt="image du panier" id="panier"/>
+			<strong>Panier</strong>
+		</a>
+	</p>
+</article>
