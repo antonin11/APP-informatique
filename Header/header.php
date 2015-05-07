a80ae3777106c42bfe1bbcd1267116357bf11580
MathieuGi <math.girardon@hotmail.fr>
2015-05-07 09:41:50 +0200

    header
    suppression fichier doublon

--- Header/header.php
+++ Header/header.php
@@ -1,11 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<link rel="stylesheet" href="style_header.css" type="text/css" />
-</head>
-<header>
-        
+	<header>	
 		<article id="log">
 			<p>
 				<a href="accueil.php">
@@ -18,32 +11,10 @@
 				<p>
 					<input type="search" name="search" placeholder="recherche" size="50" maxlength="50"/>
 				</p>
-			</from>
+			</form>
 		</div>
 		<section>		
-			<article>
-				<p>
-					<form action="traitement.php" method="post">
-						<input type="email" name="email" placeholder="Email"/>
-						<input type="password" name="pass" placeholder="Mot de passe"/>
-						<input type="submit" value="Se connecter"/>
-					</forme>
-				</p>
-				<p class="complement_connection">
-					<a href="formulaire_inscription.php"> Pas encore inscrit ? </a>
-				</p>
-				<p class="complement_connection">
-					<a href="récupérer_mot_de_passe.php"> Mot de passe oublié ? </a>
-				</p>
-			</article>	
-			<aside>
-				<p>
-					<a href="##"> 
-						<img src="images/panier.png" alt="image du panier" id="panier"/>
-						<strong>Panier</strong>
-					</a>
-				</p>
-			</aside>
+			
 		</section>
 		<nav>
 			<ul class="menu">
@@ -53,5 +24,4 @@
 				<li><a href="#">Mes annonces</a></li>
 			</ul>
 		</nav>
-
-</header>
+	</header>
