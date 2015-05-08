ed9d035fa3e979ae17e96a710d916d613900000a
MathieuGi <math.girardon@hotmail.fr>
2015-05-08 12:54:20 +0200

    connection
    dev

--- Header/connection_connecte.php
+++ Header/connection_connecte.php
@@ -1,27 +1,6 @@
 <article>
 	<p>
-		Bienvenu <?php try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=I_NEED_EAT;charset=utf8', 'root', '');
-	}
-	catch (Exception $e)
-	{
-		die('erreur :' . $e->getMessage());
-	}
-	// On récupère tout le contenu de la table jeux_video
-$reponse = $bdd->query('SELECT * FROM utilisateurs WHERE Pseudo=\'Benkouze\'');
-
-// On affiche chaque entrée une à une
-while ($donnees = $reponse->fetch())
-{
-?>
-    <p>
-   bienvenu sur votre Compte <?php echo $donnees['Prénom']; ?> <?php echo $donnees['Nom']; ?>
-   </p>
-<?php
-}
-
-$reponse->closeCursor(); // Termine le traitement de la requête
+		Bienvenu <?php echo $_SESSION['pseudo'];
 	?>
 	</p>
 	<a href="#"> 
