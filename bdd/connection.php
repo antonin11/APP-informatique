285b311ed284d77ac5cef97574b187341ea569ac
Gatty <simongatty3@gmail.com>
2015-05-11 14:07:07 +0200

    Simon
    Connection base de données

--- bdd/connection.php
+++ bdd/connection.php
@@ -2,7 +2,7 @@
 $bdd=NULL;
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=I_NEED_EAT; charset=utf8','root','');
+	$bdd = new PDO('mysql:host = localhost; dbname = I_NEED_EAT; charset=utf8','root','root');
 }
 catch (Exception $e)
 {
