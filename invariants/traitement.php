258ce0fc401640e7c1407eef05767db280ac884c
antonin11 <pedotti.antonin@gmail.com>
2015-05-20 11:36:17 +0200

    Suppression après sauvegarde

--- invariants/traitement.php
+++
@@ -1,4 +0,0 @@
-<?php session_start();
-	$_SESSION['pseudo']=NULL;
-header('location: ../accueil/accueil.php'); 
-?>
--- suppression/supprimer.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include("../bdd/connection.php");
-$req = $bdd -> prepare('DELETE FROM annonces WHERE id_annonce = ?');
-$req ->execute(array($_GET['annonce']));
-$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
-		header('Location: ' . $referer);
-		$reponse->closeCursor();
-?>
